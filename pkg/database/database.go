02f285ae033f7c37942647b9945cf2c28990770e
Kevin KADOSH <kevin@crowdsec.net>
2025-12-04 17:24:31 +0100

    remove log debug

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -13,7 +13,7 @@ type Client struct {
 
 func NewClient(sqliteDBPath string) (*Client, error) {
 	client := &Client{}
-	db, err := gorm.Open(sqlite.Open(sqliteDBPath), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	db, err := gorm.Open(sqlite.Open(sqliteDBPath), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
 	if err != nil {
 		return client, err
 	}
